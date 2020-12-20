83d8ac9ec67f19bada2fdde4f783fe4f79a0237a
Alec Sammon <alec.sammon@mangahigh.com>
2020-12-20 11:50:59 +0000

    Switch to autoloader

--- clubcloud-video/index.php
+++ clubcloud-video/index.php
@@ -1,14 +1,10 @@
 <?php
-include("./models/ClubCloudVideoPlugin.php");
-include("./models/ClubCloudVideoPlugin_Admin.php");
-include("./models/ClubCloudVideoPlugin_JWT.php");
-include("./models/ClubCloudVideoPlugin_Shortcode.php");
 
 /**
  * Plugin Name:     ClubCloud Video
  * Plugin URI:      https://clubcloud.tech
  * Description:     Allows embedding of the ClubCloud Video App into Wordpress
- * Version:         0.2-alpha
+ * Version:         0.3-alpha
  * Requires         PHP: 7.4
  * Author:          Alec Sammon, Craig Jones
  * Author URI:      https://clubcloud.tech/
@@ -21,6 +17,16 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 if ( ! class_exists( 'ClubCloudVideo_Plugin' ) ) {
+
+	function clubCloudVideo_autoloader( $className ) {
+		if ( strpos( $className, 'ClubCloudVideo' ) === 0 ) {
+			$srcLocation = realpath( plugin_dir_path( __FILE__ ) ) . DIRECTORY_SEPARATOR;
+
+			$fileName = str_replace( '_', DIRECTORY_SEPARATOR, $className ) . '.php';
+			require_once($srcLocation . $fileName);
+		}
+	}
+	spl_autoload_register( 'clubCloudVideo_autoloader' );
+
 	add_action( 'plugins_loaded', [ 'ClubCloudVideoPlugin', 'init' ] );
-	// ---
 }
