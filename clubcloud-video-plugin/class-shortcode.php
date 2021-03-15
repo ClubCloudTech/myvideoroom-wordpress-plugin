ad6d7037a568d4f9646b2c9e2541be962d4d8224
Alec Sammon <alec.sammon@mangahigh.com>
2021-03-15 10:34:30 +0000

    Fix file paths

--- clubcloud-video-plugin/class-shortcode.php
+++ clubcloud-video-plugin/class-shortcode.php
@@ -20,7 +20,7 @@ abstract class Shortcode {
 	 * @return string
 	 */
 	protected function get_plugin_version(): string {
-		$plugin_data = get_plugin_data( __DIR__ . '/../index.php' );
+		$plugin_data = get_plugin_data( __DIR__ . '/index.php' );
 
 		return $plugin_data['Version'];
 	}
