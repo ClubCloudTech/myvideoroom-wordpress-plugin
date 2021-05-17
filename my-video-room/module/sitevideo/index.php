af2dc1f1bfd004c70c71301e182c8d931a2d6cce
Fred Baumhardt <fred@acsd.uk>
2021-05-17 16:57:05 +0000

    Update

--- my-video-room/module/sitevideo/index.php
+++ my-video-room/module/sitevideo/index.php
@@ -7,12 +7,9 @@
 
 declare( strict_types=1 );
 
-<<<<<<< HEAD
 namespace MyVideoRoomPlugin\Module\SiteVideo;
 
-=======
 use MyVideoRoomPlugin\Factory;
->>>>>>> Next batch of cleanup post arrival
 use MyVideoRoomPlugin\Library\Module;
 use MyVideoRoomPlugin\Module\SiteVideo\MVRSiteVideo;
 
@@ -20,22 +17,14 @@ use MyVideoRoomPlugin\Module\SiteVideo\MVRSiteVideo;
 	'myvideoroom_init',
 	function () {
 		Module::register(
-<<<<<<< HEAD
-			'site-video',
-			\esc_html__( 'Site Video', 'myvideoroom' ),
+			'siteconference',
+			\esc_html__( 'Site Conference Center', 'myvideoroom' ),
 			array(
 				\esc_html__(
-					'The site wide video room is available for team wide meetings, events, or any need for a central room at the website level. This permanent room is created automatically by the module at activation, and can be renamed and removed. It is secured such that any normal site administrator is a host of the room. Room permissions, reception settings, templates, and custom reception videos are all available for the room.',
-=======
-			'siteconference',
-			'Site Conference Center',
-			array(
-				esc_html__(
 					'The site conference module suite is available for team wide meetings, events, or any need for central rooms at the website level.
 					These permanent rooms are created automatically by the module, at activation, and can be renamed. They can be individually secured
 					such that any site role group can host the room. Room permissions, reception settings, templates, and custom reception videos
 					are all available for each conference room. You can connect permanent WebRTC enabled devices like projectors, and microphones to rooms permanently',
->>>>>>> Next batch of cleanup post arrival
 					'myvideoroom'
 				),
 			),
