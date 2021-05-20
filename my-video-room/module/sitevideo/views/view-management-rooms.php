f0fdb8be47a4ae44b53b5eb8c254a36e50124ef7
Alec Sammon <alec.sammon@googlemail.com>
2021-05-20 19:03:58 +0100

    Fix tabs (#38)

--- my-video-room/module/sitevideo/views/view-management-rooms.php
+++ my-video-room/module/sitevideo/views/view-management-rooms.php
@@ -3,7 +3,7 @@
  * Renders the form for changing the user video preference.
  *
  * @param string|null $current_user_setting
- * @param array $available_layouts
+ * @param array       $available_layouts
  *
  * @package MyVideoRoomPlugin\Module\SiteVideo
  */
@@ -12,15 +12,18 @@ use MyVideoRoomPlugin\Factory;
 use MyVideoRoomPlugin\DAO\RoomMap;
 use MyVideoRoomPlugin\Library\Dependencies;
 use MyVideoRoomPlugin\Core\Shortcode\UserVideoPreference;
-use \MyVideoRoomPlugin\Core\SiteDefaults;
+use MyVideoRoomPlugin\Core\SiteDefaults;
 use MyVideoRoomPlugin\Entity\MenuTabDisplay;
+use MyVideoRoomPlugin\Library\HTML as HTML;
+use MyVideoRoomPlugin\Module\Security\Shortcode\SecurityVideoPreference as SecurityVideoPreference;
 use MyVideoRoomPlugin\Module\SiteVideo\Library\MVRSiteVideoListeners;
 use MyVideoRoomPlugin\Module\SiteVideo\MVRSiteVideo;
 
-return function(
+return function (
 	int $room_id,
 	string $input_type = null
-	): string {
+): string {
+	$html_library = Factory::get_instance( HTML::class, array( 'view-management' ) );
 
 	ob_start();
 
@@ -36,7 +39,6 @@ return function(
 
 	// Rendering Only Default Config Page.
 
-	//phpcs:ignore --WordPress.Security.NonceVerification.Recommended - Not needed as only using it as a flag - no processing.
 	if ( 'admin' === $input_type ) {
 		echo '<div class="mvr-nav-shortcode-outer-wrap mvr-security-room-host">';
 		//phpcs:ignore --WordPress.Security.EscapeOutput.OutputNotEscaped - Function already Escaped.
@@ -46,17 +48,10 @@ return function(
 			array( 'basic', 'premium' )
 		);
 		echo '</div>';
+
 		return ob_get_clean();
 	}
 
-	//phpcs:ignore --WordPress.Security.NonceVerification.Recommended . Its a global not user input.
-	if ( null !== ( esc_textarea( wp_unslash( $_GET['id'] ?? '' ) ) ) ) {
-
-	} else {
-		echo 'No Room ID Provided - exiting';
-		wp_safe_redirect( get_site_url() );
-		exit;
-	}
 	$room_object = Factory::get_instance( RoomMap::class )->get_room_info( $room_id );
 	$room_name   = $room_object->room_name;
 	if ( ! $room_name ) {
@@ -65,42 +60,52 @@ return function(
 
 	$base_menu = new MenuTabDisplay();
 	$base_menu->set_tab_display_name( esc_html__( 'Room Hosts', 'my-video-room' ) )
-	->set_tab_slug( 'roomhosts' )
-	->set_function_callback(
-		Factory::get_instance( MyVideoRoomPlugin\Module\Security\Shortcode\SecurityVideoPreference::class )->choose_settings( $room_id, $room_name . Dependencies::MULTI_ROOM_HOST_SUFFIX, null, 'roomhost' )
-	);
+		->set_tab_slug( 'roomhosts' )
+		->set_function_callback(
+			Factory::get_instance( SecurityVideoPreference::class )->choose_settings(
+				$room_id,
+				$room_name . Dependencies::MULTI_ROOM_HOST_SUFFIX,
+				null,
+				'roomhost'
+			)
+		);
 	$base_option  = array( $base_menu );
 	$output_array = apply_filters( 'myvideoroom_sitevideo_admin_page_menu', $base_option, $room_id );
 	?>
-			<nav class="nav-tab-wrapper myvideoroom-nav-tab-wrapper">
-				<ul class="menu">
-					<?php
-					$active = '-active';
-					foreach ( $output_array as $menu_output ) {
-						$tab_display_name = $menu_output->get_tab_display_name();
-						$tab_slug         = $menu_output->get_tab_slug();
-						?>
-						<a class="nav-tab nav-tab<?php echo esc_textarea( $active ); ?>" href="#<?php echo esc_textarea( $tab_slug ); ?>"><?php echo esc_textarea( $tab_display_name ); ?> </a>
-						<?php
-						$active = null;
-					}
-					?>
-				</ul>
-			</nav>
-
+	<nav class="nav-tab-wrapper myvideoroom-nav-tab-wrapper">
+		<ul>
 			<?php
-			foreach ( $output_array as $article_output ) {
-				$function_callback = $article_output->get_function_callback();
-				$tab_slug          = $article_output->get_tab_slug();
+			$active = 'nav-tab-active';
+			foreach ( $output_array as $menu_output ) {
+				$tab_display_name = $menu_output->get_tab_display_name();
+				$tab_slug         = $menu_output->get_tab_slug();
 				?>
-				<article id="<?php echo esc_textarea( $tab_slug ); ?> ">
+				<li>
+					<a class="nav-tab <?php echo esc_attr( $active ); ?>" href="#<?php echo esc_attr( $html_library->get_id( $tab_slug ) ); ?>">
+						<?php echo esc_html( $tab_display_name ); ?>
+					</a>
+				</li>
 				<?php
-				// phpcs:ignore --WordPress.Security.EscapeOutput.OutputNotEscaped - callback escaped within itself.
-				echo $function_callback;
-				?>
-				</article>	
-
-				<?php
+				$active = null;
 			}
+			?>
+		</ul>
+	</nav>
+
+	<?php
+	foreach ( $output_array as $article_output ) {
+		$function_callback = $article_output->get_function_callback();
+		$tab_slug          = $article_output->get_tab_slug();
+		?>
+		<article id="<?php echo esc_attr( $html_library->get_id( $tab_slug ) ); ?>">
+			<?php
+			// phpcs:ignore --WordPress.Security.EscapeOutput.OutputNotEscaped - callback escaped within itself.
+			echo $function_callback;
+			?>
+		</article>
+
+		<?php
+	}
+
 	return ob_get_clean();
 };
