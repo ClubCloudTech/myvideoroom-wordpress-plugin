2c6a787751eeaaabcf2eff838d1b29d608ed5589
Alex Dale <alex@clubcloud.tech>
2021-07-12 08:41:15 +0000

    removed strong tags from translated stuff

--- my-video-room/library/class-shortcodedocuments.php
+++ my-video-room/library/class-shortcodedocuments.php
@@ -203,7 +203,7 @@ class ShortcodeDocuments {
 					<td style="width:75%; text-align: left;"><strong>
 							<?php
 							esc_html_e(
-								'Use this Shortcode wherever possible to render <strong>Personal Meetings</strong>. Renders the Main Site Meeting Center Reception page for users. This page is automatically created by the plugin in the details above, but can also be added anywhere on the site. Please note that this switch automatically changes the host and guest context depending on user state (logged on/off/admins etc). Take special care when using this page with regards to emails- invites etc. The page contains filters in the host for anonymous meeting invites, querying users etc. We recommend using the default Meeting Center location for emails, invites etc. The plugins own template and widgets always use the default location of the Meeting Center, which you can change on this tab without issue.',
+								'Use this Shortcode wherever possible to render Personal Meetings. Renders the Main Site Meeting Center Reception page for users. This page is automatically created by the plugin in the details above, but can also be added anywhere on the site. Please note that this switch automatically changes the host and guest context depending on user state (logged on/off/admins etc). Take special care when using this page with regards to emails- invites etc. The page contains filters in the host for anonymous meeting invites, querying users etc. We recommend using the default Meeting Center location for emails, invites etc. The plugins own template and widgets always use the default location of the Meeting Center, which you can change on this tab without issue.',
 								'my-video-room'
 							);
 							?>
@@ -216,7 +216,7 @@ class ShortcodeDocuments {
 					<td style="width:75%; text-align: left;">
 						<?php
 						esc_html_e(
-							'This shortcode will always render the <strong>Guest</strong> reception of the meeting center. It will prompt the user for the username of the Host, accept a meeting invite link (automatically in the URL), or accept a hostname (automatically in the URL) It will also prompt for the Site Video Room if enabled. <strong>Please note</strong>- this link is not meant to be used for BuddyPress, WCFM, or WooCommerce Bookings pages which	use their own logic. Please use the shortcodes in BuddyPress, WCFM, WooCommerce Bookings, etc for placing on plugin pages.',
+							'This shortcode will always render the Guest reception of the meeting center. It will prompt the user for the username of the Host, accept a meeting invite link (automatically in the URL), or accept a hostname (automatically in the URL) It will also prompt for the Site Video Room if enabled. Please note- this link is not meant to be used for BuddyPress, WCFM, or WooCommerce Bookings pages which	use their own logic. Please use the shortcodes in BuddyPress, WCFM, WooCommerce Bookings, etc for placing on plugin pages.',
 							'my-video-room'
 						);
 						?>
@@ -229,7 +229,7 @@ class ShortcodeDocuments {
 					<td style="width:75%; text-align: left;">
 						<?php
 						esc_html_e(
-							'This shortcode will always render the <strong>Host</strong> reception of the meeting center. This page determines its host from the logged in user. If placed in anonymous/non-logged in areas of the site the shortcode will default to guest reception mode. <strong>Please note</strong> this link is not meant to be used for BuddyPress, WCFM, or WooCommerce Bookings pages which use their own logic. Please use the shortcodes in BuddyPress, WCFM, WooCommerce Bookings, etc for placing on plugin pages. Host settings render automatically in the short code or can be rendered separately by using the [personalmeetinghostsettings] shortcode',
+							'This shortcode will always render the Host reception of the meeting center. This page determines its host from the logged in user. If placed in anonymous/non-logged in areas of the site the shortcode will default to guest reception mode. Please note this link is not meant to be used for BuddyPress, WCFM, or WooCommerce Bookings pages which use their own logic. Please use the shortcodes in BuddyPress, WCFM, WooCommerce Bookings, etc for placing on plugin pages. Host settings render automatically in the short code or can be rendered separately by using the [personalmeetinghostsettings] shortcode',
 							'my-video-room'
 						);
 						?>
@@ -241,7 +241,7 @@ class ShortcodeDocuments {
 					<td style="width:75%; text-align: left;">
 						<?php
 						esc_html_e(
-							'This shortcode will render only the <strong>settings</strong> page of the <strong>Host</strong>. This is useful if you just want to edit the room settings without launching the full room. This shortcode determines its host from the logged in user. If placed in anonymous/non-logged in areas of the site the shortcode will return blank. <strong>Please note</strong> admin settings for personal rooms are shared between BuddyPress Profile Rooms and Personal Video Rooms as they are effectively the same room, with multiple entrances ',
+							'This shortcode will render only the settings page of the Host. This is useful if you just want to edit the room settings without launching the full room. This shortcode determines its host from the logged in user. If placed in anonymous/non-logged in areas of the site the shortcode will return blank. Please note admin settings for personal rooms are shared between BuddyPress Profile Rooms and Personal Video Rooms as they are effectively the same room, with multiple entrances ',
 							'my-video-room'
 						);
 						?>
